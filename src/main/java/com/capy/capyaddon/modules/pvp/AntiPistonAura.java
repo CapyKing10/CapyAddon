8a55e5bb99f945b580983e01ec7f4011253d6669
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-09-21 11:00:04 -0500

    antipisonaura render

--- src/main/java/com/capy/capyaddon/modules/pvp/AntiPistonAura.java
+++ src/main/java/com/capy/capyaddon/modules/pvp/AntiPistonAura.java
@@ -1,12 +1,14 @@
 package com.capy.capyaddon.modules.pvp;
 
 import com.capy.capyaddon.CapyAddon;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.meteorclient.utils.player.PlayerUtils;
+import meteordevelopment.meteorclient.utils.render.color.Color;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.decoration.EndCrystalEntity;
@@ -18,6 +20,7 @@ import net.minecraft.util.math.Direction;
 import com.capy.capyaddon.utils.cPlaceBreakUtils;
 import net.minecraft.util.profiling.jfr.event.PacketReceivedEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AntiPistonAura extends Module {
@@ -36,8 +39,33 @@ public class AntiPistonAura extends Module {
         .defaultValue(true)
         .build()
     );
+    private final Setting<Boolean> obsidianRender = sgGeneral.add(new BoolSetting.Builder()
+        .name("render")
+        .description("Renders an overlay where blocks will be placed.")
+        .defaultValue(true)
+        .build()
+    );
+    private final Setting<ShapeMode> obsidianShapeMode = sgGeneral.add(new EnumSetting.Builder<ShapeMode>()
+        .name("shape-mode")
+        .description("How the shapes are rendered.")
+        .defaultValue(ShapeMode.Both)
+        .build()
+    );
+    private final Setting<SettingColor> obsidianSideColor = sgGeneral.add(new ColorSetting.Builder()
+        .name("side-color")
+        .description("The side color of the target block rendering.")
+        .defaultValue(new SettingColor(0, 255, 0, 60))
+        .build()
+    );
+    private final Setting<SettingColor> obsidianLineColor = sgGeneral.add(new ColorSetting.Builder()
+        .name("line-color")
+        .description("The line color of the target block rendering.")
+        .defaultValue(new SettingColor(0, 255, 0, 190))
+        .build()
+    );
 
     BlockPos placelocation = null;
+    private final List<BlockPos> obsidianPos = new ArrayList<>();
 
     public AntiPistonAura() {
         super(CapyAddon.PVP, "AntiPistonAura", "prevents niggas from being faggots");
@@ -79,5 +107,14 @@ public class AntiPistonAura extends Module {
             }
         }
         return null;
+    }
+    @EventHandler
+    private void onRender(Render3DEvent event) {
+        if (obsidianRender.get() && placelocation != null) {
+            Color sideColor = obsidianSideColor.get();
+            Color lineColor = obsidianLineColor.get();
+
+            event.renderer.box(placelocation, sideColor, lineColor, obsidianShapeMode.get(), 0);
+        }
     }
 }
